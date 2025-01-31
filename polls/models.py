ba90ba2d1672ab80dc67ec3def43119cefbcc26a
Unai Muñoz <unaimunoz2024@gmail.com>
2025-01-31 17:01:45 +0100

    Segundo commit Django

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,7 @@
+import datetime
 from django.db import models
+from django.utils import timezone
+from django.contrib import admin
 
 
 class Question(models.Model):
@@ -6,6 +9,14 @@ class Question(models.Model):
     pub_date = models.DateTimeField("date published")
     def __str__(self):
         return self.question_text
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
@@ -14,3 +25,11 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
